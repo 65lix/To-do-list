73f586b98a0f6fa2530e443361043fc7b5ec44df
Felix <1b3def@gmail.com>
2022-12-30 19:58:43 +0530

    first comit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyparser=require("body-parser");
+const app= express();
+
+var items=[];
+
+
+app.set("view engine","ejs");
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(express.static("public"))
+
+
+app.get("/",function(req,res){
+   var today=new Date();     
+
+   var option={
+        weekday:"long", 
+        day:"numeric",
+        month:"long"
+   };
+
+   var day=today.toLocaleDateString("en-us",option);
+
+   res.render("list",{
+    kindofday:day,newitem:items
+   });
+
+   app.post("/",function(req,res){
+    var a=req.body.duty;
+    items.push(a);
+    res.redirect("/");
+   })
+            
+});
+
+
+
+ 
+app.listen(3000,function(){
+    console.log("server is running");
+});
